b522a5c5ee89cc1f859ed5c367d405789419ae6a
StankevichSergey <stankevi4sa@gmail.com>
2013-11-12 13:18:46 -0500

    View my plan page added

---
+++ WebApplication1/WebApplication1/ViewMyPlan.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ViewMyPlan.aspx.cs" Inherits="WebApplication1.ViewMyPlan" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:DropDownList ID="ddlCustomer" runat="server" OnSelectedIndexChanged="ddlCustomer_SelectedIndexChanged" AutoPostBack="true" EnableViewState="true">
+    </asp:DropDownList>
+    <br />
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+        
+    </asp:GridView>
+    </asp:Content>
---
+++ WebApplication1/WebApplication1/ViewMyPlan.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class ViewMyPlan : System.Web.UI.Page
+    {
+        string strCon = ConfigurationManager.ConnectionStrings["MyNutritionConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection oCon = new SqlConnection(strCon);
+            SqlCommand cmd = new SqlCommand("select CustID,FirstName from Customer",oCon);
+            oCon.Open();
+            //cmd.Parameters.Add(ddlCustomer.se);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                ListItem oItem = new ListItem();
+                oItem.Text = dr["FirstName"].ToString();
+                oItem.Value=dr["CustID"].ToString();
+                ddlCustomer.Items.Add(oItem);
+                //ddlCustomer.s
+            }
+        }
+
+        protected void ddlCustomer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<Plans> lstPlans = new List<Plans>();
+            SqlConnection oCon = new SqlConnection(strCon);
+            SqlCommand cmd = new SqlCommand("Select PlanID,CreatedDate,LastModifiedDate,Categories,PlanDesc,Tracked from Plans where CustID=@CustID", oCon);
+            cmd.Parameters.AddWithValue("@CustID", ddlCustomer.SelectedValue);
+            oCon.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Plans oPlans = new Plans();
+                oPlans.PlanID =Convert.ToInt32(dr["PlanID"].ToString());
+                oPlans.CreatedDate = dr["CreatedDate"].ToString();
+                oPlans.LastModifiedDate = dr["LastModifiedDate"].ToString();
+                oPlans.Categories = dr["Categories"].ToString();
+                oPlans.PlanDesc = dr["PlanDesc"].ToString();
+                oPlans.Tracked = dr["Tracked"].ToString();
+                lstPlans.Add(oPlans);
+            }
+            GridView1.DataSource = lstPlans;
+            GridView1.DataBind();
+
+        }
+    }
+
+    public class Plans
+    {
+        public int PlanID { get; set; }
+        public string CreatedDate { get; set; }
+        public string LastModifiedDate { get; set; }
+        public string Categories { get; set; }
+        public string PlanDesc { get; set; }
+        public string Tracked { get; set; }
+    }
+}
