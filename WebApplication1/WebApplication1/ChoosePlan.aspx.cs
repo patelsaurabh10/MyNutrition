ce0d7534bda8409c3241f6dc5cfec3c7fa907cba
Amir-Reza-Singh <maxamirjoon@gmail.com>
2013-11-22 01:56:10 -0500

    ChoosePlan Test Ver.1

--- WebApplication1/WebApplication1/ChoosePlan.aspx.cs
+++ WebApplication1/WebApplication1/ChoosePlan.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplication1.App_Code;
 
 namespace WebApplication1
 {
@@ -24,145 +25,21 @@ namespace WebApplication1
         protected void BtnLoose_Click(object sender, EventArgs e)
         {
             calorie = bmr - 500;
-            if (calorie < 1300)
-            {
-                PlanID = 1;
-                
-            }
-            else if (calorie >= 1300 && calorie < 1500)
-            {
-                PlanID = 2;
-                
-            }
-            else if (calorie >= 1500 && calorie < 1700)
-            {
-                PlanID = 3;
-                
-            }
-            else if (calorie >= 1700 && calorie < 1900)
-            {
-                PlanID = 4;
-            }
-            else if (calorie >= 1900 && calorie < 2100)
-            {
-                PlanID = 5;
-            }
-            else if (calorie >= 2100 && calorie < 2300)
-            {
-                PlanID = 6;
-            }
-            else if (calorie >= 2300 && calorie < 2500)
-            {
-                PlanID = 7;
-            }
-            else if (calorie >= 2500 && calorie < 2700)
-            {
-                PlanID = 8;
-            }
-            else if (calorie >= 2700 && calorie < 2900)
-            {
-                PlanID = 9;
-            }
-            else if (calorie >= 2900)
-            {
-                PlanID = 10;
-            }
+            PlanID = Plan.choosePlanID(calorie);
             Label1.Text = Convert.ToString(PlanID);
         }
 
         protected void BtnMaintain_Click(object sender, EventArgs e)
         {
             calorie = bmr;
-            if (calorie < 1300)
-            {
-                PlanID = 1;
-            }
-            else if (calorie >= 1300 && calorie < 1500)
-            {
-                PlanID = 2;
-            }
-            else if (calorie >= 1500 && calorie < 1700)
-            {
-                PlanID = 3;
-                
-                
-            }
-            else if (calorie >= 1700 && calorie < 1900)
-            {
-                PlanID = 4;
-            }
-            else if (calorie >= 1900 && calorie < 2100)
-            {
-                PlanID = 5;
-            }
-            else if (calorie >= 2100 && calorie < 2300)
-            {
-                PlanID = 6;
-            }
-            else if (calorie >= 2300 && calorie < 2500)
-            {
-                PlanID = 7;
-            }
-            else if (calorie >= 2500 && calorie < 2700)
-            {
-                PlanID = 8;
-            }
-            else if (calorie >= 2700 && calorie < 2900)
-            {
-                PlanID = 9;
-            }
-            else if (calorie >= 2900)
-            {
-                PlanID = 10;
-            }
+            PlanID = Plan.choosePlanID(calorie);
             Label1.Text = Convert.ToString(PlanID);
         }
 
         protected void BtnGain_Click(object sender, EventArgs e)
         {
             calorie = bmr + 500;
-            if (calorie < 1300)
-            {
-                PlanID = 1;                
-            }
-            else if (calorie >= 1300 && calorie < 1500)
-            {
-                PlanID = 2;
-            }
-            else if (calorie >= 1500 && calorie < 1700)
-            {
-                PlanID = 3;
-            }
-            else if (calorie >= 1700 && calorie < 1900)
-            {
-                PlanID = 4;
-            }
-            else if (calorie >= 1900 && calorie < 2100)
-            {
-                PlanID = 5;
-                
-            }
-            else if (calorie >= 2100 && calorie < 2300)
-            {
-                PlanID = 6;
-            }
-            else if (calorie >= 2300 && calorie < 2500)
-            {
-                PlanID = 7;
-            }
-            else if (calorie >= 2500 && calorie < 2700)
-            {
-                PlanID = 8;
-            }
-            else if (calorie >= 2700 && calorie < 2900)
-            {
-                PlanID = 9;
-            }
-            else if (calorie >= 2900)
-            {
-                PlanID = 10;
-            }
-            
+            PlanID = Plan.choosePlanID(calorie);        
             Label1.Text = Convert.ToString(PlanID);
         }
 
