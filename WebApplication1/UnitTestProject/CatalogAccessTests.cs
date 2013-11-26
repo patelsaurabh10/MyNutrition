392b206bb06aaca900cfa8594ec894fc9b046a4e
Amir-Reza-Singh <maxamirjoon@gmail.com>
2013-11-26 02:44:43 -0500

    CatalogAccess Test Ver. 2

--- WebApplication1/UnitTestProject/CatalogAccessTests.cs
+++ WebApplication1/UnitTestProject/CatalogAccessTests.cs
@@ -11,11 +11,7 @@ namespace UnitTestProject
     public class CatalogAccessTests
     {
 
-        [TestMethod]
-        public void getCustomersTEST()
-        {
-
-        }
+        
         [TestMethod]
         public void getPlanDescTEST()
         {
@@ -27,6 +23,43 @@ namespace UnitTestProject
             Assert.AreEqual(planDescExpected, planDescActual);
             
             
+        }
+        [TestMethod]
+        public void getPlanDescTEST2()
+        {
+            int PlanID = 3;
+            string planDescExpected = "for 1600 calories";
+
+
+            string planDescActual = CatalogAccess.getPlanDesc(PlanID);
+            Assert.AreEqual(planDescExpected, planDescActual);
+
+
+        }
+
+        [TestMethod]
+        public void getPlanDescTEST3()
+        {
+            int PlanID = 6;
+            string planDescExpected = "for 2200 calories";
+
+
+            string planDescActual = CatalogAccess.getPlanDesc(PlanID);
+            Assert.AreEqual(planDescExpected, planDescActual);
+
+
+        }
+        [TestMethod]
+        public void getPlanDescTEST4()
+        {
+            int PlanID = 9;
+            string planDescExpected = "for 2800 calories";
+
+
+            string planDescActual = CatalogAccess.getPlanDesc(PlanID);
+            Assert.AreEqual(planDescExpected, planDescActual);
+
+
         }
 
         [TestMethod]
@@ -40,6 +73,36 @@ namespace UnitTestProject
         }
 
         [TestMethod]
+        public void getFoodCalorieTEST2()
+        {
+            string foodName = "Chicken shawarma";
+            decimal calorieExpected = 520;
+            decimal calorieActual = CatalogAccess.getFoodCalorie(foodName);
+            Assert.AreEqual(calorieExpected, calorieActual);
+
+        }
+
+        [TestMethod]
+        public void getFoodCalorieTEST3()
+        {
+            string foodName = "Blackberry banana shake";
+            decimal calorieExpected = 290;
+            decimal calorieActual = CatalogAccess.getFoodCalorie(foodName);
+            Assert.AreEqual(calorieExpected, calorieActual);
+
+        }
+
+        [TestMethod]
+        public void getFoodCalorieTEST4()
+        {
+            string foodName = "BBQ chicken with ceasar salad";
+            decimal calorieExpected = 540;
+            decimal calorieActual = CatalogAccess.getFoodCalorie(foodName);
+            Assert.AreEqual(calorieExpected, calorieActual);
+
+        }
+
+        [TestMethod]
         public void getFoodUnitTEST()
         {
             string foodName = "Rice while cooked";
@@ -47,13 +110,18 @@ namespace UnitTestProject
             double unitActual = CatalogAccess.getFoodUnit(foodName);
             Assert.AreEqual(unitExpected, unitActual);
         }
-
-
         [TestMethod]
-        public void getGridViewSumCalorieTEST()
+        public void getFoodUnitTEST2()
         {
-            
+            string foodName = "Onion rings";
+            double unitExpected = 113;
+            double unitActual = CatalogAccess.getFoodUnit(foodName);
+            Assert.AreEqual(unitExpected, unitActual);
         }
+      
+
+
+       
         [TestMethod]
         public void getMealIDTEST()
         {
@@ -67,6 +135,33 @@ namespace UnitTestProject
             
 
         }
+        [TestMethod]
+        public void getMealIDTEST2()
+        {
+            String Day = "Tuesday";
+            int PlanID = 2;
+            List<int> MealIDsExpected = new List<int>() { 43, 44, 45, 46, 47 };
+
+            List<int> MealdIDsActual = CatalogAccess.getMealID(Day, PlanID);
+
+            CollectionAssert.AreEqual(MealIDsExpected, MealdIDsActual);
+
+
+        }
+
+        [TestMethod]
+        public void getMealIDTEST3()
+        {
+            String Day = "Friday";
+            int PlanID = 4;
+            List<int> MealIDsExpected = new List<int>() { 132, 133, 134, 135, 136, 137 };
+
+            List<int> MealdIDsActual = CatalogAccess.getMealID(Day, PlanID);
+
+            CollectionAssert.AreEqual(MealIDsExpected, MealdIDsActual);
+
+
+        }
 
         [TestMethod]
         public void GetCustomerPlanIDTEST()
@@ -76,6 +171,10 @@ namespace UnitTestProject
             int PlanIDActual = CatalogAccess.GetCustomerPlanID(customerID);
             Assert.AreEqual(PlanIDExpected, PlanIDActual);
         }
+
+       
+       
+
         [TestMethod]
         public void getFoodIDIDTEST()
         {
@@ -87,6 +186,17 @@ namespace UnitTestProject
         }
 
         [TestMethod]
+        public void getFoodIDIDTEST2()
+        {
+            string FoodName = "Onion rings";
+            int FoodIDExpected = 24;
+
+            int FoodIDActual = CatalogAccess.getFoodID(FoodName);
+            Assert.AreEqual(FoodIDExpected, FoodIDActual);
+        }
+
+
+        [TestMethod]
         public void getMealCalorieTEST()
         {
             string day = "Monday";
@@ -96,5 +206,37 @@ namespace UnitTestProject
             double calorieActual = CatalogAccess.getMealCalorie(day, PlanID, MealType);
             Assert.AreEqual(calorieExpected, calorieActual);
         }
+
+        [TestMethod]
+        public void deleteFoodInMealTEST()
+        {
+            string FoodName = "Cereal-oats";
+            int MealID = 33;
+            int aExpected = 1;
+            int aActual = CatalogAccess.deleteFoodInMeal(FoodName, MealID);
+            Assert.AreEqual(aExpected, aActual);
+        }
+        [TestMethod]
+        public void insert_Into_FoodDetailTEST()
+        {
+            int MealID = 56;
+            int FoodID = 15;
+            double Quantity = 1;
+
+            int aEpected = 1;
+            int aActual = CatalogAccess.insert_Into_FoodDetail(MealID, FoodID, Quantity);
+            Assert.AreEqual(aEpected, aActual);
+        }
+        [TestMethod]
+        public void insert_Into_FoodDetailWithWeightTEST()
+        {
+            int MealID = 56;
+            int FoodID = 14;
+            double Weight = 240;
+            int aEpected = 1;
+            int aActual = CatalogAccess.insert_Into_FoodDetail(MealID, Weight, FoodID);
+            Assert.AreEqual(aEpected, aActual);
+        }
+        
     }
 }
