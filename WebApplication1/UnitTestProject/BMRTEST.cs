e3bba4ac3a11e4966a4df4111442cd3993e1c821
tianyuzhangkirk <tianyuz@yahoo.com>
2013-12-01 17:55:45 -0800

    The BMR PARTS with unit tests

---
+++ WebApplication1/UnitTestProject/BMRTEST.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1;
+using WebApplication1.App_Code;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    class BMRTEST
+    {
+        [TestMethod]
+        public void getPlanDescTEST()
+        {
+            double BMR = 0;
+            double height = 180;
+            double weight = 150;
+            int age = 25;
+            string gender = "Male";
+            var bmr = new bmrch();
+            double valueexpected = 3116.5;
+            BMR = bmr.BMRCal_Click(height, weight, age, gender);
+            Assert.AreEqual(valueexpected, BMR);
+            
+           
+        }
+    }
+}
