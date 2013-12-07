b6e15ef60b67f8604787bb23d3f4a4b16573b258
Amir-Reza-Singh <maxamirjoon@gmail.com>
2013-12-06 21:37:38 -0500

    UI fix

--- WebApplication1/WebApplication1/App_Code/dlPlan.cs
+++ WebApplication1/WebApplication1/App_Code/dlPlan.cs
@@ -393,7 +393,7 @@ namespace WebApplication1
             SqlConnection conn = GetConnection(builder);
             conn.Open();
             SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "select CreatedDate from CustomerPlan";
+            cmd.CommandText = "SELECT MIN([CreatedDate]) FROM [MyNutrition].[dbo].[CustomerPlan]";
             DateTime dt1 = (DateTime)cmd.ExecuteScalar();
         
 
